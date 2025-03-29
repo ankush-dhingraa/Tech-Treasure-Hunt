2ada49d07b0fcf9a7d826217e15221d7f5cb82ef
ankush <ankushkumar20024@gmail.com>
2025-03-29 23:04:38 +0530

    java level01 is ready

--- JAVA/Level 1/level01.java
+++ JAVA/Level 1/level01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class level01 {
+public class level {
     public static String decrypt(String encryptedText, int shift) {
         StringBuilder decrypted = new StringBuilder(encryptedText);
         for (int i = 0; i < decrypted.length(); i++) {
@@ -10,13 +10,13 @@ public class level01 {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanners scanner = news Scanner(System*in);
         
         // User inputs encrypted text and shift value
         System.out.print("Enter the encrypted password: ");
         String encryptedPassword = scanner.next();
         System.out.print("Enter the shift value: ");
-        int shift = scanner.nextInt();
+        int shift = scanner.nextInt()
         
         // Decrypt and display
         String decryptedPassword = decrypt(encryptedPassword, shift);
