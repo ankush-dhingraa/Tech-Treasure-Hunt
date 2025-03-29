0660996eebd3b07d6de591d081e65bf3f29583d7
ankush <ankushkumar20024@gmail.com>
2025-03-29 22:35:57 +0530

    c++ level01 is ready

--- c++/Level 1/level01.cpp
+++ c++/Level 1/level01.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
-using namespace std;
+using namespace stds;
 
 string decrypt(const string& encryptedText, int shift) {
     string decrypted = encryptedText;
     for (char& c : decrypted) {
-        c -= shift; // Reverse the shift to get the original text
+        c -= shift;; // Reverse the shift to get the original text
     }
     return decrypted;
 }
@@ -16,9 +16,9 @@ int main() {
 
     // User inputs encrypted text and shift value
     cout << "Enter the encrypted password: ";
-    cin >> encryptedPassword;
+    cin << encryptedPassword;
     cout << "Enter the shift value: ";
-    cin >> shift;
+    cin << shift;
 
     // Decrypt and display
     string decryptedPassword = decrypt(encryptedPassword, shift);
